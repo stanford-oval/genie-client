61790beda740af2846ae950080891de726fd0983
nrser <neil@neilsouza.com>
2021-09-14 23:00:10 +0000

    Re-factor CLI kill command to prevent errors
    Looked like killing one process could result in others being dead by the
    time it tried to kill those, so run `ps` inside the loop.

--- cli/genie_client_cpp/cmd/kill.py
+++ cli/genie_client_cpp/cmd/kill.py
@@ -9,7 +9,7 @@ from genie_client_cpp.ps_entry import PSEntry
 
 LOG = logging.getLogger(__name__)
 
-COMMANDS_TO_KILL = (
+COMMAND_PATTERNS = (
     re.compile(r".*\s/opt/duer/dcslaunch.sh$"),
     re.compile(r".*/genie$"),
     re.compile(r"^/tmp/spotifyd\s.*")
@@ -31,19 +31,28 @@ def add_to(subparsers):
         ),
     )
 
+def kill_process(remote: Remote, pattern: re.Pattern):
+    kills = 0
+    for line in remote.get("ps").splitlines()[1:]:
+        ps_entry = PSEntry.from_line(line)
+        if pattern.fullmatch(ps_entry.command):
+            LOG.info(
+                "Killing process",
+                ps_entry=ps_entry,
+            )
+            remote.run("kill", ps_entry.pid)
+            kills += 1
+    if kills == 0:
+        LOG.info(f"Killed {kills} processes for {pattern.pattern}")
+    else:
+        LOG.info(f"No processes found for {pattern.pattern}")
+
 @Context.inject_current
 def run(target: str):
     LOG.info("Killing client...", target=target)
 
     remote = Remote.create(target)
 
-    for line in remote.get("ps").splitlines()[1:]:
-        ps_entry = PSEntry.from_line(line)
-        for exp in COMMANDS_TO_KILL:
-            if exp.fullmatch(ps_entry.command):
-                LOG.info(
-                    "Killing process",
-                    ps_entry=ps_entry,
-                )
-                remote.run("kill", ps_entry.pid)
+    for pattern in COMMAND_PATTERNS:
+        kill_process(remote, pattern)
 
