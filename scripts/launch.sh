938022706bc4a2bc518d377a1ff80ef84d1be1b7
ad31c0 <ad31c0@users.noreply.github.com>
2021-09-17 21:32:28 +0200

    Fix launch script to run pulseaudio

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -9,10 +9,12 @@ amixer -D hw:audiocodec cset name='LINEOUT volume' 8
 amixer -D hw:audiocodec cset name='MIC1 gain volume' 6
 amixer -D hw:audiocodec cset name='MIC2 gain volume' 6
 
-export GIO_EXTRA_MODULES=lib/gio/modules
-export LD_LIBRARY_PATH=lib/:/lib:/usr/lib
+export GIO_EXTRA_MODULES=/opt/genie/lib/gio/modules
+export LD_LIBRARY_PATH=/opt/genie/lib/:/lib:/usr/lib
 export GST_REGISTRY_UPDATE=no
 export GST_PLUGIN_PATH=/opt/genie/lib/gstreamer-1.0
 export GST_PLUGIN_SCANNER=/opt/genie/lib/gstreamer-1.0/gst-plugin-scanner
-sleep 5
+if [ -z "$(pidof pulseaudio)" ]; then
+	./pulseaudio -D -v -F /opt/genie/.system.pa -p /opt/genie/lib/pulseaudio --exit-idle-time=-1 --log-target=file:/tmp/pa.log
+fi
 ./genie
