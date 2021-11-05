ea6e701ae5bb717598086f029e0168f8c54499da
ad31c0 <ad31c0@users.noreply.github.com>
2021-11-05 18:54:00 +0100

    tuning PA ec params and volume levels

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -6,8 +6,8 @@ if [ -z "$(pidof ntpd)" ]; then
 	/usr/bin/start_ntp.sh &
 fi
 amixer -D hw:audiocodec cset name='LINEOUT volume' 3
-amixer -D hw:audiocodec cset name='MIC1 gain volume' 6
-amixer -D hw:audiocodec cset name='MIC2 gain volume' 6
+amixer -D hw:audiocodec cset name='MIC1 gain volume' 6.5
+amixer -D hw:audiocodec cset name='MIC2 gain volume' 6.5
 
 export GIO_EXTRA_MODULES=/opt/genie/lib/gio/modules
 export LD_LIBRARY_PATH=/opt/genie/lib/pulseaudio:/opt/genie/lib/:/lib:/usr/lib
