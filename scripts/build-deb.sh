18d1c7a13522dab3ffc265beca775e6c5f54d935
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-12-06 15:06:41 -0800

    Use systemd-nspawn to build on raspbian
    Using the official raspbian image

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -3,22 +3,39 @@
 set -e
 set -x
 
-mkdir -p ./debian/out
-for arch in ${ARCH:-arm32v7 arm32v6 arm64v8 amd64} ; do
-    if test ${arch} = arm32v6 ; then
-        CXXFLAGS="-g -O2 -ffile-prefix-map=/src=. -fstack-protector-strong -Wformat -Werror=format-security -march=armv6+fp -marm"
-        BASE_IMAGE=arm32v7
-    else
-        CXXFLAGS="-g -O2 -ffile-prefix-map=/src=. -fstack-protector-strong -Wformat -Werror=format-security"
-        BASE_IMAGE=${arch}
-    fi
+DEBIAN_RELEASE=bullseye
 
-    docker build --build-arg "ARCH=${arch}" --build-arg "CXXFLAGS=${CXXFLAGS}" \
-        --build-arg "BASE_IMAGE=${BASE_IMAGE}" \
+# normal debian build
+
+outdir=./debian/out/debian/${DEBIAN_RELEASE}
+mkdir -p ${outdir}
+for arch in ${ARCH:-arm32v7 arm64v8 amd64} ; do
+    BASE_IMAGE=${arch}/debian:${DEBIAN_RELEASE}-slim
+    docker build --build-arg "ARCH=${arch}" --build-arg "BASE_IMAGE=${BASE_IMAGE}" \
         -f scripts/Dockerfile.debian -t localhost/genie-builder:debian-${arch} .
-    docker run --rm -v ./debian/out:/out --entrypoint bash --security-opt label=disable \
+
+    docker run --rm -v ${outdir}:/out --entrypoint bash --security-opt label=disable \
         localhost/genie-builder:debian-${arch} -c 'cp /genie-client* /out'
-    if test ${arch} = arm32v7 ; then
-        rename armhf armv7 debian/out/*
-    fi
 done
+(cd ${outdir} ; dpkg-scanpackages -m . /dev/null > Packages)
+gzip -k ${outdir}/Packages
+
+# raspios build
+RASPIOS_VERSION=${2021-10-30}
+
+outdir=./debian/out/raspios/${DEBIAN_RELEASE}
+mkdir -p ${outdir}
+if ! test -f debian/out/raspios-${DEBIAN_RELEASE}.img ; then
+    wget https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-11-08/${RASPIOS_VERSION}-raspios-${DEBIAN_RELEASE}-armhf-lite.zip
+    unzip ${RASPIOS_VERSION}-raspios-${DEBIAN_RELEASE}-armhf-lite.zip
+    mv ${RASPIOS_VERSION}-raspios-${DEBIAN_RELEASE}-armhf-lite.img debian/out/raspios-${DEBIAN_RELEASE}.img
+    rm -f ${RASPIOS_VERSION}-raspios-${DEBIAN_RELEASE}-armhf-lite.zip
+    qemu-img resize -f raw debian/out/raspios-${DEBIAN_RELEASE}.img 10G
+    parted debian/out/raspios-${DEBIAN_RELEASE}.img rm 1 resizepart 2 11G set 2 boot on
+fi
+builddir=`mktemp -d`
+sudo systemd-nspawn --image debian/out/raspios-${DEBIAN_RELEASE}.img \
+    --overlay $(pwd):${builddir}:/src --bind $(realpath ${outdir}):/out \
+    /src/scripts/build-raspios.sh
+(cd ${outdir} ; dpkg-scanpackages -m . /dev/null > Packages)
+gzip -k ${outdir}/Packages
